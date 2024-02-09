0b74e22af4b4591bad2f51aa4d46508e5069a88f
S <30459885+tacticstactics@users.noreply.github.com>
2024-02-09 14:24:40 +0900

    1

--- mach_zender_interferometer_time_def.py
+++ mach_zender_interferometer_time_def.py
@@ -21,27 +21,23 @@ def beamsplitter(PT,Ein):
     #Phi: Phase Shift
 
     #Dielectric Beamsplitter
-     #phiT = 0
-     #phiR = 0
-     #phiO = 0
-
-
-     #Symmetric Beamsplitter
-     phiT = 0
-     phiR = -0.5 * np.pi    
-     phiO = 0.5 * np.pi
-
-
-     T = np.sqrt(PT) # Transmission defined as Electric field
-
-     PR = 1-PT 
-
-     R = np.sqrt(PR) # Reflection defined as Electric field
-
-     Theta1 = np.arctan(R/T) #Radian   
-         
-     BSmatrix1 = np.dot(np.exp(1J*phiO),np.array([[np.sin(Theta1)*np.exp(1J*phiR),np.cos(Theta1)*np.exp(-1J*phiT)],[np.cos(Theta1)*np.exp(1j*phiT),-1*np.sin(Theta1)*np.exp(-1J*phiR)]]))
-     
-     Eout = np.dot(BSmatrix1, Ein)
-
-     return Eout
+    #phiT = 0
+    #phiR = 0
+    #phiO = 0
+    #Symmetric Beamsplitter
+    phiT = 0
+    phiR = -0.5 * np.pi    
+    phiO = 0.5 * np.pi
+    
+    T = np.sqrt(PT) # Transmission defined as Electric field
+    
+    PR = 1-PT 
+    
+    R = np.sqrt(PR) # Reflection defined as Electric field
+    
+    Theta1 = np.arctan(R/T) #Radian
+    
+    BSmatrix1 = np.dot(np.exp(1J*phiO),np.array([[np.sin(Theta1)*np.exp(1J*phiR),np.cos(Theta1)*np.exp(-1J*phiT)],[np.cos(Theta1)*np.exp(1j*phiT),-1*np.sin(Theta1)*np.exp(-1J*phiR)]]))
+    Eout = np.dot(BSmatrix1, Ein)
+    
+    return Eout
